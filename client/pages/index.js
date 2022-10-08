cc225805c8ad5f455ef5031148ae41efddea571c
ccarver80 <ckc_80@outlook.com>
2022-10-07 23:25:51 -0600

    work on homepage

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import woodSign from '../styles/woodSign.png'
 
 export default function Home() {
   return (
-   <div>
-    <h1>TBJ WoodWorking and Tinkering</h1>
-   </div>
+
+    <div className='bg-woodBox bg-center bg-cover h-screen '>
+    <div className='flex h-screen'>
+    <div className='flex flex-col mx-auto my-auto font-bold text-white bg-slate-900/50 w-1/2 p-5 rounded-xl lg:text-4xl'>
+      <h1 className='mx-auto'>IB Lazy J WoodWorking </h1>
+      <h1 className='mx-auto'>And Tinkering</h1></div>
+    </div>
+    </div>
+   
+      
+
+
+   
+    
+
   )
 }
---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'woodworking': "url('../styles/woodworking.jpg')" ,
+        'woodSign': "url('../styles/woodSign.png')",
+        'woodBox': "url('../styles/j.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
